65d1d3f2dedaa1757960e0b90cc83ce43552778c
rasibley <rasibley@redhat.com>
2019-02-06 15:33:52 -0500

    initial commit of network driver sanity test

---
+++ networking/driver/sanity/README.md
@@ -0,0 +1,12 @@
+# Network Driver Santy Test
+Test Maintainer: [John Linville](mailto:linville@redhat.com)
+
+## How to run it
+
+### Dependencies
+Please refer to the top-level README.md for common dependencies. 
+
+### Execute the test
+```bash
+$ make run
+```
---
+++ networking/driver/sanity/runtest.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+. /usr/bin/rhts_environment.sh
+
+COUNT=0
+
+#
+# Count number of devices under /sys/class/net with type == 1 (ethernet)
+#
+echo -e "\n****Running : Counting the number of ethernet devices"
+for netdev in /sys/class/net/*
+do
+	if [ "$(cat ${netdev}/type)" == "1" ]
+	then
+		COUNT=$(($COUNT + 1))
+	fi
+done
+        echo "$COUNT ethernet devices were found"
+
+# 
+# If none found, assume failure to load any ethernet driver and fail test
+#
+if [ $COUNT -eq 0 ]
+then
+        echo "FAIL: No ethernet devices found"
+        report_result $TEST FAIL 1
+        exit
+fi
+
+#
+# For each device under /sys/class/net...
+#
+echo -e "\n****Running : 'ethtool -i' against each ethernet device"
+for netdev in /sys/class/net/*
+do
+        #
+        # Check if type == 1 (ethernet)
+        #
+        if [ "$(cat ${netdev}/type)" == "1" ]
+        then
+                #
+                # If so, try to run "ethtool -i" on the device.
+                #
+                dev=$(basename $netdev);
+                echo -e "\nRunning ethtool -i on $dev"
+                ethtool -i $dev
+
+                #
+                # If ethtool returns failure, then fail the test...
+                #
+                if [ $? -ne 0 ];
+                then
+		        echo "FAIL: ethtool -i returned a failure"
+		        report_result $TEST FAIL 1
+                fi
+        fi
+done
+
+#
+# Otherwise, success!
+#
+report_result $TEST PASS 0
+exit
