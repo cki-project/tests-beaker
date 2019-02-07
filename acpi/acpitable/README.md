58a9d4077f083cf830c4b3e5ed5d28454a447af2
Fendy Tjahjadi <ftjahjad@redhat.com>
2019-02-07 11:25:52 -0500

    initial files

---
+++ acpi/acpitable/README.md
@@ -0,0 +1,11 @@
+#ACPI table test suite
+Testing ACPI table is present in sysfs. \
+Test Maintainer: [Al Stone](mailto:ahs3@redhat.com)
+
+## How to run it
+Please refer to the top-level README.md for common dependencies. Test-specific dependencies will automatically be installed when executing 'make run'. For a complete detail, see PURPOSE file.
+
+### Execute the test
+```bash
+$ make run
+```
---
+++ acpi/acpitable/runtest.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------
+# Copyright (c) 2019 Red Hat, Inc. All rights reserved. This copyrighted material 
+# is made available to anyone wishing to use, modify, copy, or
+# redistribute it subject to the terms and conditions of the GNU General
+# Public License v.2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Author: Al Stone <ahs3@redhat.com>
+#---------------------------------------------------------------------------------
+#
+#	Simple test: are ACPI tables present?
+#
+#	There's no foolproof method to know, but we can check to see
+#	if acpidump can read files from /sys/firmware/acpi/tables.  An
+#	added bonus is that we capture a copy of the tables, too.
+#---------------------------------------------------------------------------------
+
+# Source the common test script helpers
+. /usr/bin/rhts-environment.sh || exit 1
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+# make sure acpica-tools are installed
+pkg=$(rpm -qa | grep acpica-tools)
+[ -z "$pkg" ] && exit 1
+
+# run acpidump, which should succeed and write tables to stdout
+# (requires being run as root)
+acpidump
+[ $? != 0 ] && exit 1
+
+# all is well
+exit 0
