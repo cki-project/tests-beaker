cf952bc8b2a3e5270a23fda0ce82158f08cd1bd9
Fendy Tjahjadi <ftjahjad@redhat.com>
2019-02-21 13:29:25 -0500

    inital commint of i2c sanity test

---
+++ packages/i2c-tools/sanity/i2cdetect-smoke/runtest.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# vim: dict=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/i2c-tools/Sanity/i2cdetect-smoke
+#   Description: Loads i2c-dev module, tries i2cdetect -l. If something is detected probes for more info.
+#   Author: Lukas Zachar <lzachar@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2012 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include Beaker environment
+. /usr/bin/rhts-environment.sh
+. /usr/lib/beakerlib/beakerlib.sh
+
+PACKAGE="i2c-tools"
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm $PACKAGE
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "modprobe i2c-dev"
+        rlRun "i2cdetect -l > list 2>list.err"
+        cat list
+        for I2C in $(sed 's/^i2c-\([0-9]\+\).*/\1/' list); do
+            rlRun "i2cdetect -F $I2C > $I2C-f.txt 2> $I2C-f.err"
+            rlRun "i2cdetect -y $I2C > $I2C-y.txt 2> $I2C-y.err"
+        done
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "modprobe -r i2c-dev"
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
