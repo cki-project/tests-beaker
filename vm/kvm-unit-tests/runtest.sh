00c1f300864ef41988c2d379ea1d6a8525b0266f
Marcelo Condotta <mcondotta@redhat.com>
2019-06-04 19:20:20 -0300

    Enable KVM Unit Tests to run on Apollo ARM hardware

--- vm/kvm-unit-tests/runtest.sh
+++ vm/kvm-unit-tests/runtest.sh
@@ -26,7 +26,7 @@ function check_virt_support
     elif [[ $hwpf == "aarch64" ]]; then
         dmesg | egrep -iq "kvm"
         if (( $? == 0 )); then
-            dmesg | egrep -iq "kvm.*: Hyp mode initialized successfully"
+            dmesg | egrep -iq "kvm.*: (Hyp|VHE) mode initialized successfully"
         else
             #
             # XXX: Note that the harness (i.e. beaker) does clear dmesg, hence
@@ -34,7 +34,7 @@ function check_virt_support
             #      "journalctl -k"
             #
             journalctl -k | \
-                egrep -iq "kvm.*: Hyp mode initialized successfully"
+                egrep -iq "kvm.*: (Hyp|VHE) mode initialized successfully"
         fi
         return $?
     elif [[ $hwpf == "ppc64" || $hwpf == "ppc64le" ]]; then
