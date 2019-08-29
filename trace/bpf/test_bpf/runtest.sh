1ccb23cacbff4886ed9afe5ea93b46843eab9d29
Zamir SUN <zsun@redhat.com>
2019-08-30 02:48:08 +0800

    trace/bpf: Improve support for debug kernel (#336)
    * trace/bpf: Improve support for debug kernel
    
    Signed-off-by: Ziqian SUN (Zamir) <zsun@redhat.com>
    
    * test_bpf: Workaround cross-compile kernel-devel issue
    
    Signed-off-by: Ziqian SUN (Zamir) <zsun@redhat.com>

--- trace/bpf/test_bpf/runtest.sh
+++ trace/bpf/test_bpf/runtest.sh
@@ -20,27 +20,33 @@ DistName=`rpm -E %{?dist} | sed 's/[.0-9]//g'`
 DistVer=`rpm -E %{?dist} | sed 's/[^0-9]//g'`
 DUPARCH=`arch`
 KVer=`uname -r | awk -F '-' '{print $1}'`
-KDIST=`uname -r | sed "s/.$(arch)//g" | awk -F '.' '{print "."$NF}'`
-KBUILD=`uname -r | awk -F '-' '{print $2}' | sed "s/.$(arch)//g" | sed "s/${KDIST}//g"`
+KDIST=`uname -r | sed "s/.$(arch)//g;s/\+debug//g" | awk -F '.' '{print "."$NF}'`
+KBUILD=`uname -r | awk -F '-' '{print $2}' | sed "s/.$(arch)//g;s/\+debug//g" | sed "s/${KDIST}//g"`
 KBuild=${KBuild:-${KBUILD}}
 KBuildPrefix=`echo ${KBuild} | awk -F '.' '{print $1}'`
 
-kernel_nvr=`uname -r | sed "s/.$(arch)//g"`
-KVARIANT=`rpm -q --queryformat '%{sourcerpm}\n' -qf /boot/config-$(uname -r) | sed "s/.srpm//g;s/.src.rpm//g;s/-${kernel_nvr}//g"`
+kernel_vra=`uname -r | sed "s/+debug//g"`
+KNAME=`rpm -q --queryformat '%{sourcerpm}\n' -qf /boot/config-$(uname -r) | sed "s/.srpm//g;s/.src.rpm//g;s/-${KVer}-${KBuild}${KDIST}//g"`
+KVARIANT=""
+if [[ $(uname -r) == *"debug"* ]]; then
+    KVARIANT="-debug"
+fi
 
 yum=$(select_yum_tool)
 
 rlJournalStart
     rlPhaseStartSetup
-        for i in libgcc glibc-static gcc gcc-c++ kernel-devel elfutils-libelf-devel binutils-devel libcap-devel ; do
+        for i in libgcc glibc-static gcc gcc-c++ kernel-devel elfutils-libelf-devel binutils-devel libcap-devel openssl-devel ; do
             $yum install -y $i
         done
         # nfs-utils git util-linux createrepo genisoimage gcc gcc-c++ rpm-build kernel-abi-whitelists wget python-setuptools
         # yum install -y elfutils-libelf-devel binutils-devel newt-devel python-devel perl xmlto asciidoc perl-ExtUtils-Embed
-        $yum download kernel-devel-$(uname -r)
-        $yum download kernel-${kernel_nvr} --source
-        rpm -ivh --force kernel-devel-$(uname -r).rpm
+        $yum install -y kernel${KVARIANT}-devel-${kernel_vra} || $yum download kernel${KVARIANT}-devel-${kernel_vra} || rlRpmDownload kernel${KVARIANT}-devel-${kernel_vra}
+        $yum download kernel-${KVer}-${KBuild}${KDIST} --source || rlRpmDownload --source ${KNAME}-${KVer}-${KBuild}${KDIST}
+
+        rpm -q kernel${KVARIANT}-devel-${kernel_vra} || rpm -ivh --force kernel-devel-${kernel_vra}.rpm
         rpm -ivh --force $(rpm -q --queryformat '%{sourcerpm}\n' -qf /boot/config-$(uname -r))
+
         $yum install -y kmod
 
         tar Jxf ~/rpmbuild/SOURCES/linux-${KVer}-${KBuild}*.tar.xz
@@ -49,10 +55,16 @@ rlJournalStart
         mkdir test_bpf
         find . -name test_bpf.c -exec cp {} test_bpf \;
         echo 'obj-m+= test_bpf.o' > test_bpf/Makefile
+        pushd linux-${KVer}-${KBuild}${KDIST}
+            cp /boot/config-$(uname -r) .config
+            make oldconfig
+            make prepare
+            make modules_prepare
+        popd
         pushd test_bpf
         BEAHARCH=${ARCH}
         unset ARCH
-        make -C /lib/modules/`uname -r`/build M=`pwd` modules
+        make -C ../linux-${KVer}-${KBuild}${KDIST}/ M=`pwd` modules
         popd
         rmmod test_bpf
         ARCH=${BEAHARCH}
