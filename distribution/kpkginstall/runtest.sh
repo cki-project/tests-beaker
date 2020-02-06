3c2f8da6459b58612ba8a668be6be278fbcd4fb5
Rachel Sibley <rasibley@redhat.com>
2020-02-06 10:47:33 -0500

    Make kpkginstall compatible with RHEL5
    Older versions of bash don't support certain syntax, update cki_lib
    to make it compatible for rhel5. Default to yumdownloader command
    as --download option was added RHEL6 or later.

--- distribution/kpkginstall/runtest.sh
+++ distribution/kpkginstall/runtest.sh
@@ -313,7 +313,7 @@ function copr_prepare()
 function download_install_package()
 {
   # If download of a package fails, report warn/abort -> infrastructure issue
-  if $YUM install --downloadonly -y $1 > /dev/null; then
+  if $YUM install --downloadonly -y $1 > /dev/null || yumdownloader -y $1 > /dev/null; then
     cki_print_success "Downloaded $1 successfully"
   else
     cki_abort_recipe "Failed to download ${1}!" WARN
@@ -462,22 +462,21 @@ else
   if [ -f /tmp/kpkginstall/KPKG_VAR_DEBUG ]; then
     valid_kernel_versions=(
       "${KVER}.debug"           # RHEL 7 style debug kernels
-      "${KVER}.${ARCH}.debug"   # RHEL 7 style debug kernels
       "${KVER}+debug"           # RHEL 8 style debug kernels
-      "${KVER}.${ARCH}+debug"   # RHEL 8 style debug kernels
     )
   else
+    KVER=$(uname -r | sed "s/.$(uname -i)//")
     valid_kernel_versions=(
       "${KVER}"
       "${KVER}.${ARCH}"
     )
   fi
   ckver=$(uname -r)
-  cki_print_info "Acceptable kernel version strings: ${valid_kernel_versions[@]} "
+  cki_print_info "Acceptable kernel version strings: ${valid_kernel_versions[*]} "
   cki_print_info "Running kernel version string:     ${ckver}"
 
   # Did we get the right kernel running after reboot?
-  if [[ ! " ${valid_kernel_versions[@]} " =~ " ${ckver} " ]]; then
+  if [[ ! " ${valid_kernel_versions[*]} " =~ " ${ckver} " ]]; then
     cki_abort_recipe "Kernel version after reboot (${ckver}) does not match expected version strings!" WARN
   fi
 
