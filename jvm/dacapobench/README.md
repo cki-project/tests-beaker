f954598f233d690f42a8ee9ae0167f4c2eb608a3
Rachel Sibley <rasibley@redhat.com>
2020-02-13 11:39:18 -0500

    Split jvm tests (#470)
    The JVM test includes both DaCapo Benchmark and jcstress test suites.
    This test should be split into two cases, as they are distinguished
    tests suites and can hinder how we report our results in CI.

---
+++ jvm/dacapobench/README.md
@@ -0,0 +1,15 @@
+# DaCapo Benchmark Suite
+
+This benchmark suite is intended as a tool for Java benchmarking by the programming 
+language, memory management and computer architecture communities.
+http://dacapobench.sourceforge.net/
+
+Test Maintainer: [Christine Flood](mailto:chf@redhat.com)
+
+## How to run it
+Please refer to the top-level README.md for common dependencies. Test-specific dependencies will automatically be installed when executing 'make run'. For a complete detail, see PURPOSE file.
+
+### Execute the test
+```bash
+$ make run
+```
---
+++ jvm/dacapobench/runtest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------
+# Copyright (c) 2019 Red Hat, Inc. All rights reserved. This copyrighted material 
+# is made available to anyone wishing to use, modify, copy, or
+# redistribute it subject to the terms and conditions of the GNU General
+# Public License v.2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+#---------------------------------------------------------------------------------
+# This script downloads a jar test file
+# and executes the test against localhost
+
+# See https://github.com/guozheng/jmh-tutorial/blob/master/README.md
+# for more information.
+#---------------------------------------------------------------------------------
+
+# Source the common test script helpers
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+# Run DaCapo Benchmarks
+ rlPhaseStartTest
+    rlRun -l "wget https://gitlab.com/cki-project/lookaside/raw/master/dacapo-9.12-MR1-bach.jar"
+        if [ $? -ne 0 ]; then
+            rstrnt-abort -t recipe
+            exit 0
+        fi
+    rlRun -l "java -jar dacapo-9.12-MR1-bach.jar eclipse jython lusearch-fix"
+  rlPhaseEnd
+
+rlJournalEnd
+rlJournalPrintText
